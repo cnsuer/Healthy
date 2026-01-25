db389e8576203951cb6cc115571237f51952efbb
restver <restver-zg.yang@aia.com>
2026-01-25 20:47:50 +0800

    Initial Commit

---
+++ Healthy/HealthyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthyApp.swift
+//  Healthy
+//
+//  Created by Restver on 2026/1/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
